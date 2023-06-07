291838f8937983034fc5fe789e7fe185d79f2df7
Devocalpyse <ngl2d9uvg@relay.firefox.com>
2023-06-07 13:19:52 -0700

    configured app connection in 'app.ts' / included reference to taskRoutes, uncomplete

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+// Database connection:
+mongoose.connect(process.env.TEST_DB!).then(
+  () => console.log('Database connection successful!'),
+  (err) => console.log('Error connecting to the database', err)
+);
+
+// App middleware
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// TO-DO: routes, called taskRoutes
+app.use('/api/tasks', taskRoutes)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).end();
+});
+
+app.listen(3000);
